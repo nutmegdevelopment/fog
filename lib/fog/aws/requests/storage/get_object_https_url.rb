54561aaa8fa47425aa8b0db3762ac82e84102e0a
Telmo Goncalves <telmogoncalves@me.com>
2014-07-29 17:05:58 +0100

    add support for bucket name after amazon url

--- lib/fog/aws/requests/storage/get_object_https_url.rb
+++ lib/fog/aws/requests/storage/get_object_https_url.rb
@@ -1,29 +1,56 @@
 module Fog
   module Storage
     class AWS
+
       module GetObjectHttpsUrl
+
         def get_object_https_url(bucket_name, object_name, expires, options = {})
-          get_object_url(bucket_name, object_name, expires, options.merge(:scheme => 'https'))
+          unless bucket_name
+            raise ArgumentError.new('bucket_name is required')
+          end
+          unless object_name
+            raise ArgumentError.new('object_name is required')
+          end
+          host, path = if bucket_name =~ /^(?:[a-z]|\d(?!\d{0,2}(?:\.\d{1,3}){3}$))(?:[a-z0-9]|\-(?![\.])){1,61}[a-z0-9]$/
+            ["#{bucket_name}.#{@host}", object_name]
+          else
+            [@host, "#{bucket_name}/#{object_name}"]
+          end
+          https_url({
+            :headers  => {},
+            :host     => host,
+            :method   => 'GET',
+            :path     => path,
+            :query    => options[:query]
+          }, expires)
         end
+
       end
 
       class Real
+
         # Get an expiring object https url from S3
         #
-        # @param bucket_name [String] Name of bucket containing object
-        # @param object_name [String] Name of object to get expiring url for
-        # @param expires [Time] An expiry time for this url
+        # ==== Parameters
+        # * bucket_name<~String> - Name of bucket containing object
+        # * object_name<~String> - Name of object to get expiring url for
+        # * expires<~Time> - An expiry time for this url
         #
-        # @return [Excon::Response] response:
-        #   * body [String] - url for object
+        # ==== Returns
+        # * response<~Excon::Response>:
+        #   * body<~String> - url for object
         #
-        # @see http://docs.amazonwebservices.com/AmazonS3/latest/dev/S3_QSAuth.html
+        # ==== See Also
+        # http://docs.amazonwebservices.com/AmazonS3/latest/dev/S3_QSAuth.html
 
         include GetObjectHttpsUrl
+
       end
 
       class Mock # :nodoc:all
+
         include GetObjectHttpsUrl
+
       end
     end
   end
